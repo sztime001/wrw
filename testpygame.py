02fc2de0996690288e048bad6943880b550c8354
sztime001 <sofeiw0908@gmail.com>
2018-07-12 08:25:37 +1000

    Add game setting

--- testpygame.py
+++ testpygame.py
@@ -1,5 +1,33 @@
 import pygame
 
+
+def game_setting_loop(screen):
+    running = True
+    game_text = pygame.font.SysFont("Times",20)
+    game_text_instruction = "game instruction:\n press corresponding key to add \
+                             character in game.\n\n \
+                             W: Werewolf                M: Minion\n\
+                             S: Seer                    V: Villager\n\
+                             R: Robber                  T: Trouble maker\n\
+                             D: Drunk                   A: tAnner\n\
+                             O: masOn                   I: Insomniac"
+    game_text_screen = game_text.render(game_text_instruction, True, (100,100,200))
+    text_rect = game_text_screen.get_rect(center = (300, 100))
+    screen.blit(game_text_screen, text_rect)
+    pygame.display.update()
+    while running:
+        for event in pygame.event.get():
+            # only do something if the event is of type QUIT
+            if event.type == pygame.QUIT:
+                # change the value to False, to exit the main loop
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    print("key pressed")
+
+    print("setting completed")
+    return
+
 def main():
 
     # initialize the pygame module
@@ -18,14 +46,14 @@ def main():
     # create a text box shows start game
     pygame.font.init()
     game_text = pygame.font.SysFont("Times",50)
-    game_text_screen = game_text.render("Click to start game", True, (100,100,200))
+    game_text_screen = game_text.render("Press ENTER to start game...", True, (100,100,200))
     text_rect = game_text_screen.get_rect(center = (300, 250))
     screen.blit(game_text_screen, text_rect)
-    pygame.display.flip()
+    pygame.display.update()
 
     # define a variable to control the main loop
     running = True
-
+    clock = pygame.time.Clock()
     # main loop
     while running:
         # event handling, gets all event from the eventqueue
@@ -34,6 +62,12 @@ def main():
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    print("key pressed")
+                    game_setting_loop(screen)
+
+        clock.tick(10)
 
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
